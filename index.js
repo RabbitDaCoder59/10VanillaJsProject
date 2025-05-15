fda46fc258cee39c692a77313d7f1c72142f36cc
Edeh chinedu <edehchinedu59@gmail.com>
2025-05-15 17:32:32 +0100

    fix: correct timer interval and update display formatting

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const stopwatch = () => {};
 const start = () => {
   if (!isRunning) {
     startTime = Date.now() - elapsedTime;
-    timer = setInterval(update, 1000);
+    timer = setInterval(update, 10);
     isRunning = true;
   }
 };
@@ -27,8 +27,8 @@ const reset = () => {
   display.textContent = "00:00:00:00";
 };
 const update = () => {
-  const curentTime = Date.now();
-  elapsedTime = curentTime - startTime;
+  const currentTime = Date.now();
+  elapsedTime = currentTime - startTime;
 
   let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
   let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
@@ -40,5 +40,5 @@ const update = () => {
   seconds = String(seconds).padStart(2, "0");
   milliseconds = String(milliseconds).padStart(2, "0");
 
-  display.textContent = `${hours}: ${minutes}: ${seconds}: ${milliseconds}`;
+  display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 };
